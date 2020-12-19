292409b019cc7479c16ed21ee2aca7293580ec38
Baranemlek <75400163+Baranemlek@users.noreply.github.com>
2020-12-20 00:02:19 +0300

    Update blacklist.py

--- tg_bot/modules/blacklist.py
+++ tg_bot/modules/blacklist.py
@@ -147,19 +147,16 @@ def __stats__():
 __mod_name__ = "Word Blacklists"
 
 __help__ = """
-Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, \
-the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
+İnsanlar belirli şeyler söylediğinde Rose'un otomatik olarak harekete geçmesi için kara liste filtreleri ayarlayabilirsin. Bu, şunları kullanarak yapılır:
 
-*NOTE:* blacklists do not affect group admins.
+*NOT:* kara listeler grup yöneticilerini etkilemez.
 
- - /blacklist: View the current blacklisted words.
+ - /blacklist: Kara listeye alınmış mevcut kelimeleri görüntüleyin.
 
-*Admin only:*
- - /addblacklist <triggers>: Add a trigger to the blacklist. Each line is considered one trigger, so using different \
-lines will allow you to add multiple triggers.
- - /unblacklist <triggers>: Remove triggers from the blacklist. Same newline logic applies here, so you can remove \
-multiple triggers at once.
- - /rmblacklist <triggers>: Same as above.
+*sadece yönetici:*
+ - /addblacklist <tetikleyici> <sebep>: tetikleyiciyi kara listeye ekler ve sebebini ayarlar.
+ - /unblacklist <tetikleyici>: tetikleyiciyi kara listeden kaldırır.
+ - /rmblacklist <tetikleyici>: /unblacklist komutunun aynısı.<tetikleyici>: /unblacklist komutunun aynısı.
 """
 
 BLACKLIST_HANDLER = DisableAbleCommandHandler("blacklist", blacklist, filters=Filters.group, pass_args=True,
